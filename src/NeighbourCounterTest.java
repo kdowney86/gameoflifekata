d2fa3c1297d8ca9237fe2be5dc39e5ac6128bd87
Kelvin <kelvin.downey@oramon.nl>
2017-06-06 17:36:04 +0200

    Add test for two neighbours to the north and west

--- src/NeighbourCounterTest.java
+++ src/NeighbourCounterTest.java
@@ -108,4 +108,23 @@ public class NeighbourCounterTest {
         Assert.assertEquals(expected, result);
     }
 
+    @Test
+    public void countTwoNeighboursNorthAndWestTest() {
+        //given
+        test2dArrayList = new ArrayList<>();
+        test2dArrayList.add(0, new ArrayList<>(Arrays.asList(DEAD_STR, DEAD_STR, DEAD_STR, DEAD_STR)));
+        test2dArrayList.add(1, new ArrayList<>(Arrays.asList(DEAD_STR, ALIVE_STR, DEAD_STR, DEAD_STR)));
+        test2dArrayList.add(2, new ArrayList<>(Arrays.asList(ALIVE_STR, ALIVE_STR, DEAD_STR, DEAD_STR)));
+        test2dArrayList.add(3, new ArrayList<>(Arrays.asList(DEAD_STR, DEAD_STR, DEAD_STR, DEAD_STR)));
+        test2dArrayList.add(4, new ArrayList<>(Arrays.asList(DEAD_STR, DEAD_STR, DEAD_STR, DEAD_STR)));
+
+        int expected = 2;
+
+        //when
+        int result = NeighbourCounter.count(test2dArrayList, 2, 1);
+
+        //then
+        Assert.assertEquals(expected, result);
+    }
+
 }
