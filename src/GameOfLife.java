02e474a4a84f5a3ce304c716c0ee4c8ffc793d6a
Kelvin <kelvin.downey@oramon.nl>
2017-06-06 16:37:28 +0200

    Refactor

--- src/GameOfLife.java
+++ src/GameOfLife.java
@@ -7,12 +7,16 @@ public class GameOfLife {
 
     public static ArrayList<ArrayList<String>> process(ArrayList<ArrayList<String>> list) {
 
+        processCellsWithFewerThanTwoNeighbours(list);
+
+        return list;
+    }
+
+    private static void processCellsWithFewerThanTwoNeighbours(ArrayList<ArrayList<String>> list) {
         for (int i = 0; i < list.size(); i++) {
             for (int j = 0; j < list.get(i).size(); j++) {
                 list.get(i).set(j, ".");
             }
         }
-
-        return list;
     }
 }
