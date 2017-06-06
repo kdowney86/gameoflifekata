67a23b867840d6c24900de570f1c250538a0998e
Kelvin <kelvin.downey@oramon.nl>
2017-06-06 16:42:00 +0200

    Add test for NeighbourCounter when cell has no neighbours

---
+++ src/NeighbourCounter.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+/**
+ * Created by kelvin on 06/06/17.
+ */
+public class NeighbourCounter {
+
+    public static int count(ArrayList<ArrayList<String>> list, int xPos, int yPos) {
+        return -1;
+    }
+}
---
+++ src/NeighbourCounterTest.java
@@ -0,0 +1,35 @@
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Created by kelvin on 06/06/17.
+ */
+public class NeighbourCounterTest {
+
+    private ArrayList<ArrayList<String>> test2dArrayList;
+    private final String ALIVE_STR = "*";
+    private final String DEAD_STR = ".";
+    
+    @Test
+    public void countNoNeighboursTest() {
+        //given
+        test2dArrayList = new ArrayList<>();
+        test2dArrayList.add(0, new ArrayList<>(Arrays.asList(DEAD_STR, DEAD_STR, DEAD_STR, DEAD_STR)));
+        test2dArrayList.add(1, new ArrayList<>(Arrays.asList(DEAD_STR, DEAD_STR, DEAD_STR, DEAD_STR)));
+        test2dArrayList.add(2, new ArrayList<>(Arrays.asList(DEAD_STR, ALIVE_STR, DEAD_STR, DEAD_STR)));
+        test2dArrayList.add(3, new ArrayList<>(Arrays.asList(DEAD_STR, DEAD_STR, DEAD_STR, DEAD_STR)));
+        test2dArrayList.add(4, new ArrayList<>(Arrays.asList(DEAD_STR, DEAD_STR, DEAD_STR, DEAD_STR)));
+
+        int expected = 0;
+
+        //when
+        int result = NeighbourCounter.count(test2dArrayList, 2, 1);
+
+        //then
+        Assert.assertEquals(expected, result);
+    }
+
+}
