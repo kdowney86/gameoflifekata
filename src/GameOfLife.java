d64eb1ff5d74471fa03b6539b5137b0e00d7c58e
Kelvin <kelvin.downey@oramon.nl>
2017-06-06 16:16:31 +0200

    Initial Commit

---
+++ src/GameOfLife.java
@@ -0,0 +1,5 @@
+/**
+ * Created by kelvin on 06/06/17.
+ */
+public class GameOfLife {
+}
---
+++ src/GameOfLifeTests.java
@@ -0,0 +1,8 @@
+import org.junit.jupiter.api.Test;
+/**
+ * Created by kelvin on 06/06/17.
+ */
+public class GameOfLifeTests {
+
+
+}
