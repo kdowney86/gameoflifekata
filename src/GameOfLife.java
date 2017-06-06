76bcfee4d3bda6fe988d01653cf44278b2128a0c
Kelvin <kelvin.downey@oramon.nl>
2017-06-06 16:28:16 +0200

    Add failing test for live cells with fewer than two live neighbours dying

--- src/GameOfLife.java
+++ src/GameOfLife.java
@@ -1,5 +1,11 @@
+import java.util.ArrayList;
+
 /**
  * Created by kelvin on 06/06/17.
  */
 public class GameOfLife {
+
+    public static ArrayList<ArrayList<String>> process(ArrayList<ArrayList<String>> list) {
+        return null;
+    }
 }
--- src/GameOfLifeTests.java
+++ src/GameOfLifeTests.java
@@ -1,8 +1,50 @@
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Created by kelvin on 06/06/17.
  */
 public class GameOfLifeTests {
 
+    /*
+        1. Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
+        2. Any live cell with more than three live neighbours dies, as if by overcrowding.
+        3. Any live cell with two or three live neighbours lives on to the next generation.
+        4. Any dead cell with exactly three live neighbours becomes a live cell.
+     */
 
+    private GameOfLife gol;
+    private ArrayList<ArrayList<String>> test2dArrayList;
+    private final String ALIVE_STR = "*";
+    private final String DEAD_STR = ".";
+
+    @Before
+    public void setUp() {
+        gol = new GameOfLife();
+        test2dArrayList = new ArrayList<>();
+    }
+
+    @Test
+    public void testAnyLiveCellWithFewerThanTwoLiveNeighboursDies() {
+
+        test2dArrayList.add(0, new ArrayList<>(Arrays.asList(DEAD_STR, DEAD_STR, DEAD_STR, DEAD_STR)));
+        test2dArrayList.add(0, new ArrayList<>(Arrays.asList(DEAD_STR, DEAD_STR, DEAD_STR, DEAD_STR)));
+        test2dArrayList.add(0, new ArrayList<>(Arrays.asList(DEAD_STR, ALIVE_STR, DEAD_STR, DEAD_STR)));
+        test2dArrayList.add(0, new ArrayList<>(Arrays.asList(DEAD_STR, DEAD_STR, DEAD_STR, DEAD_STR)));
+        test2dArrayList.add(0, new ArrayList<>(Arrays.asList(DEAD_STR, DEAD_STR, DEAD_STR, DEAD_STR)));
+
+        ArrayList<ArrayList<String>> expectedArrayList = new ArrayList<>();
+        expectedArrayList.add(0, new ArrayList<>(Arrays.asList(DEAD_STR, DEAD_STR, DEAD_STR, DEAD_STR)));
+        expectedArrayList.add(0, new ArrayList<>(Arrays.asList(DEAD_STR, DEAD_STR, DEAD_STR, DEAD_STR)));
+        expectedArrayList.add(0, new ArrayList<>(Arrays.asList(DEAD_STR, ALIVE_STR, DEAD_STR, DEAD_STR)));
+        expectedArrayList.add(0, new ArrayList<>(Arrays.asList(DEAD_STR, DEAD_STR, DEAD_STR, DEAD_STR)));
+        expectedArrayList.add(0, new ArrayList<>(Arrays.asList(DEAD_STR, DEAD_STR, DEAD_STR, DEAD_STR)));
+
+        ArrayList<ArrayList<String>> result = GameOfLife.process(test2dArrayList);
+        Assert.assertEquals(expectedArrayList, result);
+    }
 }
