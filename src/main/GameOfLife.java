3c90d04cf0caa16be145b3bcd87f3846cb00153c
Kelvin <kelvin.downey@oramon.nl>
2017-06-12 11:37:41 +0200

    Refactor - change way cells are processed

--- src/main/GameOfLife.java
+++ src/main/GameOfLife.java
@@ -13,33 +13,25 @@ public class GameOfLife {
     public static ArrayList<ArrayList<String>> process(ArrayList<ArrayList<String>> list) {
 
         ArrayList<ArrayList<String>> resultList = new ArrayList<>();
-        fillEmptyCells(resultList, list);
         processCells(resultList, list);
 
         return resultList;
     }
 
     private static void processCells(ArrayList<ArrayList<String>> tempList, ArrayList<ArrayList<String>> originalList) {
-        for (int i = 0; i < tempList.size(); i++) {
-            for (int j = 0; j < tempList.get(i).size(); j++) {
+        for (int i = 0; i < originalList.size(); i++) {
+            ArrayList<String> lineList = new ArrayList<>();
+            for (int j = 0; j < originalList.get(i).size(); j++) {
                 if (originalList.get(i).get(j).equals(GameOfLifeConstants.ALIVE_STR) && NeighbourCounter.count(originalList, i, j) < 2)
-                    tempList.get(i).set(j, GameOfLifeConstants.DEAD_STR);
+                    lineList.add(GameOfLifeConstants.DEAD_STR);
                 else if (originalList.get(i).get(j).equals(GameOfLifeConstants.ALIVE_STR) && NeighbourCounter.count(originalList, i, j) > 3)
-                    tempList.get(i).set(j, GameOfLifeConstants.DEAD_STR);
+                    lineList.add(GameOfLifeConstants.DEAD_STR);
                 else if (originalList.get(i).get(j).equals(GameOfLifeConstants.ALIVE_STR) && NeighbourCounter.count(originalList, i, j) == 2
                         || originalList.get(i).get(j).equals(GameOfLifeConstants.ALIVE_STR) && NeighbourCounter.count(originalList, i, j) == 3)
-                    tempList.get(i).set(j, GameOfLifeConstants.ALIVE_STR);
+                    lineList.add(GameOfLifeConstants.ALIVE_STR);
+                else lineList.add(GameOfLifeConstants.DEAD_STR);
             }
-        }
-    }
-
-    private static void fillEmptyCells(ArrayList<ArrayList<String>> listToBeFilled, ArrayList<ArrayList<String>> originalList) {
-        ArrayList<String> listLine = new ArrayList<>();
-        for (int i = 0; i < originalList.get(0).size(); i++) {
-            listLine.add(GameOfLifeConstants.DEAD_STR);
-        }
-        for (int j = 0; j < originalList.size(); j++) {
-            listToBeFilled.add(listLine);
+            tempList.add(lineList);
         }
     }
 }
