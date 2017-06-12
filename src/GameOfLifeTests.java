48e98626bf75a48165260f73760fe3b812abd835
Kelvin <kelvin.downey@oramon.nl>
2017-06-12 10:12:22 +0200

    Add test for cells with more than three live neighbours

--- src/GameOfLifeTests.java
+++ src/GameOfLifeTests.java
@@ -17,11 +17,13 @@ public class GameOfLifeTests {
      */
 
     private ArrayList<ArrayList<String>> test2dArrayList;
-    private final String ALIVE_STR = "*";
-    private final String DEAD_STR = ".";
+    private final String ALIVE_STR = GameOfLifeConstants.ALIVE_STR;
+    private final String DEAD_STR = GameOfLifeConstants.DEAD_STR;
 
+    //Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
     @Test
     public void testAnyLiveCellWithFewerThanTwoLiveNeighboursDies() {
+        //given
         test2dArrayList = new ArrayList<>();
         test2dArrayList.add(0, new ArrayList<>(Arrays.asList(DEAD_STR, DEAD_STR, DEAD_STR, DEAD_STR)));
         test2dArrayList.add(1, new ArrayList<>(Arrays.asList(DEAD_STR, DEAD_STR, DEAD_STR, DEAD_STR)));
@@ -29,14 +31,34 @@ public class GameOfLifeTests {
         test2dArrayList.add(3, new ArrayList<>(Arrays.asList(DEAD_STR, DEAD_STR, DEAD_STR, DEAD_STR)));
         test2dArrayList.add(4, new ArrayList<>(Arrays.asList(DEAD_STR, DEAD_STR, DEAD_STR, DEAD_STR)));
 
-        ArrayList<ArrayList<String>> expectedArrayList = new ArrayList<>();
-        expectedArrayList.add(0, new ArrayList<>(Arrays.asList(DEAD_STR, DEAD_STR, DEAD_STR, DEAD_STR)));
-        expectedArrayList.add(1, new ArrayList<>(Arrays.asList(DEAD_STR, DEAD_STR, DEAD_STR, DEAD_STR)));
-        expectedArrayList.add(2, new ArrayList<>(Arrays.asList(DEAD_STR, DEAD_STR, DEAD_STR, DEAD_STR)));
-        expectedArrayList.add(3, new ArrayList<>(Arrays.asList(DEAD_STR, DEAD_STR, DEAD_STR, DEAD_STR)));
-        expectedArrayList.add(4, new ArrayList<>(Arrays.asList(DEAD_STR, DEAD_STR, DEAD_STR, DEAD_STR)));
+        int xPos = 2;
+        int yPos = 1;
 
+        //when
         ArrayList<ArrayList<String>> result = GameOfLife.process(test2dArrayList);
-        Assert.assertEquals(expectedArrayList, result);
+
+        //then
+        Assert.assertEquals(DEAD_STR, result.get(xPos).get(yPos));
+    }
+
+    //Any live cell with more than three live neighbours dies, as if by overcrowding.
+    @Test
+    public void testAnyLiveCellWithMoreThanThreeLiveNeighboursDies() {
+        //given
+        test2dArrayList = new ArrayList<>();
+        int xPos = 2;
+        int yPos = 1;
+        test2dArrayList.add(0, new ArrayList<>(Arrays.asList(DEAD_STR, DEAD_STR, DEAD_STR, DEAD_STR)));
+        test2dArrayList.add(1, new ArrayList<>(Arrays.asList(DEAD_STR, ALIVE_STR, DEAD_STR, DEAD_STR)));
+        test2dArrayList.add(2, new ArrayList<>(Arrays.asList(DEAD_STR, ALIVE_STR, ALIVE_STR, DEAD_STR)));
+        test2dArrayList.add(3, new ArrayList<>(Arrays.asList(DEAD_STR, ALIVE_STR, ALIVE_STR, DEAD_STR)));
+        test2dArrayList.add(4, new ArrayList<>(Arrays.asList(DEAD_STR, DEAD_STR, DEAD_STR, DEAD_STR)));
+
+
+        //when
+        ArrayList<ArrayList<String>> result = GameOfLife.process(test2dArrayList);
+
+        //then
+        Assert.assertEquals(DEAD_STR, result.get(xPos).get(yPos));
     }
 }
