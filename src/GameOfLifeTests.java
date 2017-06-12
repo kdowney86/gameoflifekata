72d1913e28782ab6353d6936de3ebfd8755f19ce
Kelvin <kelvin.downey@oramon.nl>
2017-06-12 10:47:42 +0200

    Add test for live cells with two live neighbours

--- src/GameOfLifeTests.java
+++ src/GameOfLifeTests.java
@@ -61,4 +61,25 @@ public class GameOfLifeTests {
         //then
         Assert.assertEquals(DEAD_STR, result.get(xPos).get(yPos));
     }
+
+    //Any live cell with two or three live neighbours lives on to the next generation.
+    @Test
+    public void testAnyLiveCellWithTwoLiveNeighboursLives() {
+        //given
+        test2dArrayList = new ArrayList<>();
+        int xPos = 2;
+        int yPos = 1;
+        test2dArrayList.add(0, new ArrayList<>(Arrays.asList(DEAD_STR, DEAD_STR, DEAD_STR, DEAD_STR)));
+        test2dArrayList.add(1, new ArrayList<>(Arrays.asList(DEAD_STR, DEAD_STR, DEAD_STR, DEAD_STR)));
+        test2dArrayList.add(2, new ArrayList<>(Arrays.asList(DEAD_STR, ALIVE_STR, ALIVE_STR, DEAD_STR)));
+        test2dArrayList.add(3, new ArrayList<>(Arrays.asList(DEAD_STR, ALIVE_STR, DEAD_STR, DEAD_STR)));
+        test2dArrayList.add(4, new ArrayList<>(Arrays.asList(DEAD_STR, DEAD_STR, DEAD_STR, DEAD_STR)));
+
+
+        //when
+        ArrayList<ArrayList<String>> result = GameOfLife.process(test2dArrayList);
+
+        //then
+        Assert.assertEquals(ALIVE_STR, result.get(xPos).get(yPos));
+    }
 }
