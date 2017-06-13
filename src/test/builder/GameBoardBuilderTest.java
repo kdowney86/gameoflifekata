97cd90f6411bd0a579d1a419688ef5badf52886f
Kelvin <kelvin.downey@oramon.nl>
2017-06-13 14:07:40 +0200

    Add test for larger board

--- src/test/builder/GameBoardBuilderTest.java
+++ src/test/builder/GameBoardBuilderTest.java
@@ -30,4 +30,33 @@ public class GameBoardBuilderTest {
         Assert.assertEquals(resultBoard.getWidth(), width);
         Assert.assertEquals(resultBoard.getCellValue(0, 0), GameOfLifeConstants.ALIVE_STR);
     }
+
+    @Test
+    public void test2x5BoardReturned() {
+        //given
+        ArrayList<Integer> row = new ArrayList<>();
+        row.add(1);
+        row.add(0);
+
+
+        final int height = 5;
+        final int width = 2;
+
+        //when
+        GameBoard resultBoard = new GameBoardBuilder().withRow(row).withRow(row).withRow(row).withRow(row).withRow(row).build();
+
+        //then
+        Assert.assertEquals(resultBoard.getHeight(), height);
+        Assert.assertEquals(resultBoard.getWidth(), width);
+        Assert.assertEquals(resultBoard.getCellValue(0, 0), GameOfLifeConstants.ALIVE_STR);
+        Assert.assertEquals(resultBoard.getCellValue(0, 1), GameOfLifeConstants.DEAD_STR);
+        Assert.assertEquals(resultBoard.getCellValue(1, 0), GameOfLifeConstants.ALIVE_STR);
+        Assert.assertEquals(resultBoard.getCellValue(1, 1), GameOfLifeConstants.DEAD_STR);
+        Assert.assertEquals(resultBoard.getCellValue(2, 0), GameOfLifeConstants.ALIVE_STR);
+        Assert.assertEquals(resultBoard.getCellValue(2, 1), GameOfLifeConstants.DEAD_STR);
+        Assert.assertEquals(resultBoard.getCellValue(3, 0), GameOfLifeConstants.ALIVE_STR);
+        Assert.assertEquals(resultBoard.getCellValue(3, 1), GameOfLifeConstants.DEAD_STR);
+        Assert.assertEquals(resultBoard.getCellValue(4, 0), GameOfLifeConstants.ALIVE_STR);
+        Assert.assertEquals(resultBoard.getCellValue(4, 1), GameOfLifeConstants.DEAD_STR);
+    }
 }
