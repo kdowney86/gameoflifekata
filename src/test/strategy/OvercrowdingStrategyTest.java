01bbf0c7a60531bb04acaa097892eebd42ec8de7
Kelvin <kelvin.downey@oramon.nl>
2017-06-20 14:47:17 +0200

    Move tests for each strategy and add extra tests

---
+++ src/test/strategy/OvercrowdingStrategyTest.java
@@ -0,0 +1,119 @@
+package test.strategy;
+
+import main.builder.GameBoardBuilder;
+import main.constants.GameOfLifeConstants;
+import main.model.Cell;
+import main.model.GameBoard;
+import main.strategy.impl.OvercrowdingStrategy;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Created by kelvin on 20/06/17.
+ */
+public class OvercrowdingStrategyTest {
+    private final String DEAD_STR = GameOfLifeConstants.DEAD_STR;
+    private final String ALIVE_STR = GameOfLifeConstants.ALIVE_STR;
+
+    private OvercrowdingStrategy overcrowdingStrategy = new OvercrowdingStrategy();
+    private GameBoard gameBoard;
+    private GameBoardBuilder gameBoardBuilder;
+
+    @Before
+    public void setUp() {
+        gameBoardBuilder = new GameBoardBuilder();
+    }
+
+    @Test
+    public void testCellWithThreeLiveNeighboursUnprocessed() {
+        //given
+        gameBoardBuilder = gameBoardBuilder.withRow(new ArrayList<>(Arrays.asList(DEAD_STR, DEAD_STR, DEAD_STR, DEAD_STR)));
+        gameBoardBuilder = gameBoardBuilder.withRow(new ArrayList<>(Arrays.asList(DEAD_STR, ALIVE_STR, ALIVE_STR, DEAD_STR)));
+        gameBoardBuilder = gameBoardBuilder.withRow(new ArrayList<>(Arrays.asList(DEAD_STR, ALIVE_STR, ALIVE_STR, DEAD_STR)));
+        gameBoardBuilder = gameBoardBuilder.withRow(new ArrayList<>(Arrays.asList(DEAD_STR, DEAD_STR, DEAD_STR, DEAD_STR)));
+        gameBoardBuilder = gameBoardBuilder.withRow(new ArrayList<>(Arrays.asList(DEAD_STR, DEAD_STR, DEAD_STR, DEAD_STR)));
+        gameBoard = gameBoardBuilder.build();
+
+        int rowNumber = 2;
+        int colNumber = 1;
+        Cell cell = gameBoard.getCell(rowNumber, colNumber);
+
+        //when
+        cell = overcrowdingStrategy.getNextGenerationForCell(gameBoard, rowNumber, colNumber, cell);
+
+        //then
+        Assert.assertEquals(false, cell.isProcessed());
+        Assert.assertEquals(true, cell.isAlive());
+    }
+
+    @Test
+    public void testLiveCellWithFourLiveNeighboursDies() {
+        //given
+        gameBoardBuilder = gameBoardBuilder.withRow(new ArrayList<>(Arrays.asList(DEAD_STR, DEAD_STR, DEAD_STR, DEAD_STR)));
+        gameBoardBuilder = gameBoardBuilder.withRow(new ArrayList<>(Arrays.asList(DEAD_STR, ALIVE_STR, ALIVE_STR, DEAD_STR)));
+        gameBoardBuilder = gameBoardBuilder.withRow(new ArrayList<>(Arrays.asList(DEAD_STR, ALIVE_STR, ALIVE_STR, DEAD_STR)));
+        gameBoardBuilder = gameBoardBuilder.withRow(new ArrayList<>(Arrays.asList(DEAD_STR, DEAD_STR, ALIVE_STR, DEAD_STR)));
+        gameBoardBuilder = gameBoardBuilder.withRow(new ArrayList<>(Arrays.asList(DEAD_STR, DEAD_STR, DEAD_STR, DEAD_STR)));
+        gameBoard = gameBoardBuilder.build();
+
+        int rowNumber = 2;
+        int colNumber = 1;
+        Cell cell = gameBoard.getCell(rowNumber, colNumber);
+
+        //when
+        cell = overcrowdingStrategy.getNextGenerationForCell(gameBoard, rowNumber, colNumber, cell);
+
+        //then
+        Assert.assertEquals(true, cell.isProcessed());
+        Assert.assertEquals(false, cell.isAlive());
+    }
+
+    @Test
+    public void testLiveCellWithFiveLiveNeighboursDies() {
+        //given
+        gameBoardBuilder = gameBoardBuilder.withRow(new ArrayList<>(Arrays.asList(ALIVE_STR, ALIVE_STR, DEAD_STR, DEAD_STR)));
+        gameBoardBuilder = gameBoardBuilder.withRow(new ArrayList<>(Arrays.asList(DEAD_STR, ALIVE_STR, ALIVE_STR, DEAD_STR)));
+        gameBoardBuilder = gameBoardBuilder.withRow(new ArrayList<>(Arrays.asList(DEAD_STR, ALIVE_STR, ALIVE_STR, DEAD_STR)));
+        gameBoardBuilder = gameBoardBuilder.withRow(new ArrayList<>(Arrays.asList(DEAD_STR, DEAD_STR, ALIVE_STR, DEAD_STR)));
+        gameBoardBuilder = gameBoardBuilder.withRow(new ArrayList<>(Arrays.asList(DEAD_STR, DEAD_STR, DEAD_STR, DEAD_STR)));
+        gameBoard = gameBoardBuilder.build();
+
+        int rowNumber = 1;
+        int colNumber = 1;
+        Cell cell = gameBoard.getCell(rowNumber, colNumber);
+
+        //when
+        cell = overcrowdingStrategy.getNextGenerationForCell(gameBoard, rowNumber, colNumber, cell);
+
+        //then
+        Assert.assertEquals(true, cell.isProcessed());
+        Assert.assertEquals(false, cell.isAlive());
+    }
+
+    @Test
+    public void testAlreadyProcessedCellRemainsUnaffected() {
+        //given
+        gameBoardBuilder = gameBoardBuilder.withRow(new ArrayList<>(Arrays.asList(ALIVE_STR, ALIVE_STR, DEAD_STR, DEAD_STR)));
+        gameBoardBuilder = gameBoardBuilder.withRow(new ArrayList<>(Arrays.asList(DEAD_STR, ALIVE_STR, ALIVE_STR, DEAD_STR)));
+        gameBoardBuilder = gameBoardBuilder.withRow(new ArrayList<>(Arrays.asList(DEAD_STR, ALIVE_STR, ALIVE_STR, DEAD_STR)));
+        gameBoardBuilder = gameBoardBuilder.withRow(new ArrayList<>(Arrays.asList(DEAD_STR, DEAD_STR, ALIVE_STR, DEAD_STR)));
+        gameBoardBuilder = gameBoardBuilder.withRow(new ArrayList<>(Arrays.asList(DEAD_STR, DEAD_STR, DEAD_STR, DEAD_STR)));
+        gameBoard = gameBoardBuilder.build();
+
+        int rowNumber = 1;
+        int colNumber = 1;
+        Cell cell = gameBoard.getCell(rowNumber, colNumber);
+        cell.setProcessed(true);
+
+        //when
+        cell = overcrowdingStrategy.getNextGenerationForCell(gameBoard, rowNumber, colNumber, cell);
+
+        //then
+        Assert.assertEquals(true, cell.isProcessed());
+        Assert.assertEquals(true, cell.isAlive());
+    }
+}
---
+++ src/test/strategy/SurvivalStrategyTest.java
@@ -0,0 +1,163 @@
+package test.strategy;
+
+import main.builder.GameBoardBuilder;
+import main.constants.GameOfLifeConstants;
+import main.model.Cell;
+import main.model.GameBoard;
+import main.strategy.impl.SurvivalStrategy;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Created by kelvin on 20/06/17.
+ */
+public class SurvivalStrategyTest {
+    private final String DEAD_STR = GameOfLifeConstants.DEAD_STR;
+    private final String ALIVE_STR = GameOfLifeConstants.ALIVE_STR;
+
+    private SurvivalStrategy survivalStrategy = new SurvivalStrategy();
+    private GameBoard gameBoard;
+    private GameBoardBuilder gameBoardBuilder;
+
+    @Before
+    public void setUp() {
+        gameBoardBuilder = new GameBoardBuilder();
+    }
+
+    @Test
+    public void testLiveCellWithTwoNeighboursLivesOn() {
+        //given
+        gameBoardBuilder = gameBoardBuilder.withRow(new ArrayList<>(Arrays.asList(DEAD_STR, DEAD_STR, DEAD_STR, DEAD_STR)));
+        gameBoardBuilder = gameBoardBuilder.withRow(new ArrayList<>(Arrays.asList(DEAD_STR, ALIVE_STR, ALIVE_STR, DEAD_STR)));
+        gameBoardBuilder = gameBoardBuilder.withRow(new ArrayList<>(Arrays.asList(DEAD_STR, ALIVE_STR, DEAD_STR, DEAD_STR)));
+        gameBoardBuilder = gameBoardBuilder.withRow(new ArrayList<>(Arrays.asList(DEAD_STR, DEAD_STR, DEAD_STR, DEAD_STR)));
+        gameBoardBuilder = gameBoardBuilder.withRow(new ArrayList<>(Arrays.asList(DEAD_STR, DEAD_STR, DEAD_STR, DEAD_STR)));
+        gameBoard = gameBoardBuilder.build();
+
+        int rowNumber = 1;
+        int colNumber = 1;
+        Cell cell = gameBoard.getCell(rowNumber, colNumber);
+
+        //when
+        cell = survivalStrategy.getNextGenerationForCell(gameBoard, rowNumber, colNumber, cell);
+
+        //then
+        Assert.assertEquals(true, cell.isProcessed());
+        Assert.assertEquals(true, cell.isAlive());
+    }
+
+    @Test
+    public void testLiveCellWithThreeNeighboursLivesOn() {
+        //given
+        gameBoardBuilder = gameBoardBuilder.withRow(new ArrayList<>(Arrays.asList(ALIVE_STR, ALIVE_STR, DEAD_STR, DEAD_STR)));
+        gameBoardBuilder = gameBoardBuilder.withRow(new ArrayList<>(Arrays.asList(ALIVE_STR, ALIVE_STR, DEAD_STR, DEAD_STR)));
+        gameBoardBuilder = gameBoardBuilder.withRow(new ArrayList<>(Arrays.asList(DEAD_STR, ALIVE_STR, DEAD_STR, DEAD_STR)));
+        gameBoardBuilder = gameBoardBuilder.withRow(new ArrayList<>(Arrays.asList(DEAD_STR, DEAD_STR, DEAD_STR, DEAD_STR)));
+        gameBoardBuilder = gameBoardBuilder.withRow(new ArrayList<>(Arrays.asList(DEAD_STR, DEAD_STR, DEAD_STR, DEAD_STR)));
+        gameBoard = gameBoardBuilder.build();
+
+        int rowNumber = 0;
+        int colNumber = 1;
+        Cell cell = gameBoard.getCell(rowNumber, colNumber);
+
+        //when
+        cell = survivalStrategy.getNextGenerationForCell(gameBoard, rowNumber, colNumber, cell);
+
+        //then
+        Assert.assertEquals(true, cell.isProcessed());
+        Assert.assertEquals(true, cell.isAlive());
+    }
+
+    @Test
+    public void testLiveCellWithOneNeighbourUnprocessed() {
+        //given
+        gameBoardBuilder = gameBoardBuilder.withRow(new ArrayList<>(Arrays.asList(ALIVE_STR, ALIVE_STR, DEAD_STR, DEAD_STR)));
+        gameBoardBuilder = gameBoardBuilder.withRow(new ArrayList<>(Arrays.asList(DEAD_STR, DEAD_STR, DEAD_STR, DEAD_STR)));
+        gameBoardBuilder = gameBoardBuilder.withRow(new ArrayList<>(Arrays.asList(DEAD_STR, ALIVE_STR, DEAD_STR, DEAD_STR)));
+        gameBoardBuilder = gameBoardBuilder.withRow(new ArrayList<>(Arrays.asList(DEAD_STR, DEAD_STR, DEAD_STR, DEAD_STR)));
+        gameBoardBuilder = gameBoardBuilder.withRow(new ArrayList<>(Arrays.asList(DEAD_STR, DEAD_STR, DEAD_STR, DEAD_STR)));
+        gameBoard = gameBoardBuilder.build();
+
+        int rowNumber = 0;
+        int colNumber = 0;
+        Cell cell = gameBoard.getCell(rowNumber, colNumber);
+
+        //when
+        cell = survivalStrategy.getNextGenerationForCell(gameBoard, rowNumber, colNumber, cell);
+
+        //then
+        Assert.assertEquals(false, cell.isProcessed());
+        Assert.assertEquals(true, cell.isAlive());
+    }
+
+    @Test
+    public void testLiveCellWithFourNeighboursUnprocessed() {
+        //given
+        gameBoardBuilder = gameBoardBuilder.withRow(new ArrayList<>(Arrays.asList(ALIVE_STR, ALIVE_STR, DEAD_STR, DEAD_STR)));
+        gameBoardBuilder = gameBoardBuilder.withRow(new ArrayList<>(Arrays.asList(DEAD_STR, ALIVE_STR, DEAD_STR, DEAD_STR)));
+        gameBoardBuilder = gameBoardBuilder.withRow(new ArrayList<>(Arrays.asList(ALIVE_STR, ALIVE_STR, ALIVE_STR, DEAD_STR)));
+        gameBoardBuilder = gameBoardBuilder.withRow(new ArrayList<>(Arrays.asList(DEAD_STR, ALIVE_STR, DEAD_STR, DEAD_STR)));
+        gameBoardBuilder = gameBoardBuilder.withRow(new ArrayList<>(Arrays.asList(DEAD_STR, DEAD_STR, DEAD_STR, DEAD_STR)));
+        gameBoard = gameBoardBuilder.build();
+
+        int rowNumber = 2;
+        int colNumber = 1;
+        Cell cell = gameBoard.getCell(rowNumber, colNumber);
+
+        //when
+        cell = survivalStrategy.getNextGenerationForCell(gameBoard, rowNumber, colNumber, cell);
+
+        //then
+        Assert.assertEquals(false, cell.isProcessed());
+        Assert.assertEquals(true, cell.isAlive());
+    }
+
+    @Test
+    public void testDeadCellWithFourNeighboursUnprocessed() {
+        //given
+        gameBoardBuilder = gameBoardBuilder.withRow(new ArrayList<>(Arrays.asList(ALIVE_STR, ALIVE_STR, DEAD_STR, DEAD_STR)));
+        gameBoardBuilder = gameBoardBuilder.withRow(new ArrayList<>(Arrays.asList(DEAD_STR, ALIVE_STR, DEAD_STR, DEAD_STR)));
+        gameBoardBuilder = gameBoardBuilder.withRow(new ArrayList<>(Arrays.asList(ALIVE_STR, DEAD_STR, ALIVE_STR, DEAD_STR)));
+        gameBoardBuilder = gameBoardBuilder.withRow(new ArrayList<>(Arrays.asList(DEAD_STR, ALIVE_STR, DEAD_STR, DEAD_STR)));
+        gameBoardBuilder = gameBoardBuilder.withRow(new ArrayList<>(Arrays.asList(DEAD_STR, DEAD_STR, DEAD_STR, DEAD_STR)));
+        gameBoard = gameBoardBuilder.build();
+
+        int rowNumber = 2;
+        int colNumber = 1;
+        Cell cell = gameBoard.getCell(rowNumber, colNumber);
+
+        //when
+        cell = survivalStrategy.getNextGenerationForCell(gameBoard, rowNumber, colNumber, cell);
+
+        //then
+        Assert.assertEquals(false, cell.isProcessed());
+        Assert.assertEquals(false, cell.isAlive());
+    }
+
+    @Test
+    public void testAlreadyProcessedCellUnaffected() {
+        //given
+        gameBoardBuilder = gameBoardBuilder.withRow(new ArrayList<>(Arrays.asList(ALIVE_STR, ALIVE_STR, DEAD_STR, DEAD_STR)));
+        gameBoardBuilder = gameBoardBuilder.withRow(new ArrayList<>(Arrays.asList(DEAD_STR, ALIVE_STR, DEAD_STR, DEAD_STR)));
+        gameBoardBuilder = gameBoardBuilder.withRow(new ArrayList<>(Arrays.asList(ALIVE_STR, ALIVE_STR, ALIVE_STR, DEAD_STR)));
+        gameBoardBuilder = gameBoardBuilder.withRow(new ArrayList<>(Arrays.asList(DEAD_STR, ALIVE_STR, DEAD_STR, DEAD_STR)));
+        gameBoardBuilder = gameBoardBuilder.withRow(new ArrayList<>(Arrays.asList(DEAD_STR, DEAD_STR, DEAD_STR, DEAD_STR)));
+        gameBoard = gameBoardBuilder.build();
+
+        int rowNumber = 2;
+        int colNumber = 1;
+        Cell cell = gameBoard.getCell(rowNumber, colNumber);
+        cell.setProcessed(true);
+
+        //when
+        cell = survivalStrategy.getNextGenerationForCell(gameBoard, rowNumber, colNumber, cell);
+
+        //then
+        Assert.assertEquals(true, cell.isProcessed());
+        Assert.assertEquals(true, cell.isAlive());
+    }
+}
